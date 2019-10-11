b8e67bf841133dbb204f2097ed7c0566b23bb865
Greg Werner <grjasewe@gmail.com>
2019-10-11 10:37:29 -0400

    Add files via upload
    Python .py files for project

---
+++ FHIR_Helper_Flask.py
@@ -0,0 +1,53 @@
+'''
+Created on Oct 10, 2019
+
+@author: gregory
+'''
+
+import FHIR_Helper
+
+from flask import Flask, request
+app = Flask(__name__)
+
+test_data_dict = dict()
+
+output_dict = dict()
+
+output_data_file = 'data/output.tsv';
+fhir_data_dir = 'data/fhir_stu3'
+text_list_file = 'output/text_list.tsv'
+html_lookup_file = 'data/medfind.txt'
+
+#ingest "training" data from disc
+FHIR_Helper.ingest_output_data(output_data_file)
+    
+#bring in test data from disc
+FHIR_Helper.ingest_fhir_data(fhir_data_dir)
+    
+# send this list to disc for safe keeping
+with open(text_list_file, 'w', encoding='utf-8') as fs:
+    for key in test_data_dict.keys():
+        fs.write(key + '\t' + str(test_data_dict[key]) + '\n')
+
+# make sure our html lookup list contains only unique rows
+FHIR_Helper.cleanup_html_lookup_file(html_lookup_file)
+
+
+@app.route('/suggest', methods=['POST'])
+def lookup_candidates():
+    user_query = request.form.get('query')
+    
+    more_candidates = FHIR_Helper.lookup_medlineplus(user_query, html_lookup_file)
+    if user_query not in more_candidates:
+        more_candidates.append(user_query.lower())
+        
+    return str(FHIR_Helper.find_condition_information(more_candidates))
+
+@app.route('/fetch', methods=['POST'])
+def fetch_information():
+    user_candidate = request.form.get('candidate')
+    
+    return str(FHIR_Helper.display_matching_information(user_candidate))
+
+if __name__ == '__main__':
+    app.run()
