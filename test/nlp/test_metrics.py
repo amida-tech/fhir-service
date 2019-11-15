d99231635900a5cbd30055f269fd313a144b76ac
Greg Werner <gregory@amida.com>
2019-11-15 08:32:16 -0500

    MCP-117 Linting test code

--- test/nlp/test_metrics.py
+++ test/nlp/test_metrics.py
@@ -6,54 +6,100 @@ Created on Nov 15, 2019
 
 from nlp import Metrics
 
-left_empty = []
-left_populated_1 = ['apple','banana','cherry']
-left_populated_2 = ['apple','cherry','dorian','grape','lemon']
-right_empty = []
-right_populated_1 = ['apple','banana','cherry']
-right_populated_2 = ['apple','banana','elderberry,','fig','grape','honeydew','jackfruit']
+LEFT_EMPTY = []
+LEFT_POPULATED_1 = ['apple', 'banana', 'cherry']
+LEFT_POPULATED_2 = ['apple', 'cherry', 'dorian', 'grape', 'lemon']
+RIGHT_EMPTY = []
+RIGHT_POPULATED_1 = ['apple', 'banana', 'cherry']
+RIGHT_POPULATED_2 = ['apple', 'banana', 'elderberry,', 'fig', 'grape', 'honeydew', 'jackfruit']
 
 def test_harmonic_similarity_double_empty():
-    assert(Metrics.harmonic_similarity(left_empty, right_empty) == 0.0)
+    """
+        test harmonic similarity for 2 empty lists
+    """
+    assert Metrics.harmonic_similarity(LEFT_EMPTY, RIGHT_EMPTY) == 0.0
 
 def test_harmonic_similarity_left_empty():
-    assert(Metrics.harmonic_similarity(left_empty, right_populated_1) == 0.0)
+    """
+        test harmonic similarity if the 1st parameter is empty
+    """
+    assert Metrics.harmonic_similarity(LEFT_EMPTY, RIGHT_POPULATED_1) == 0.0
 
 def test_harmonic_similarity_right_empty():
-    assert(Metrics.harmonic_similarity(left_populated_1, right_empty) == 0.0)
+    """
+        test harmonic similarity if the 2nd parameter is empty
+    """
+    assert Metrics.harmonic_similarity(LEFT_POPULATED_1, RIGHT_EMPTY) == 0.0
 
 def test_harmonic_similarity_populated_1():
-    assert(Metrics.harmonic_similarity(left_populated_1, right_populated_1) == 1.0)
+    """
+        test harmonic similarity for one pair of populated lists
+    """
+    assert Metrics.harmonic_similarity(LEFT_POPULATED_1, RIGHT_POPULATED_1) == 1.0
 
 def test_harmonic_similarity_populated_2():
-    assert(Metrics.harmonic_similarity(left_populated_2, right_populated_2) == 0.333)
+    """
+        test harmonic similarity for another pair of populated lists
+    """
+    assert Metrics.harmonic_similarity(LEFT_POPULATED_2, RIGHT_POPULATED_2) == 0.333
 
 def test_cosine_similarity_double_empty():
-    assert(Metrics.cosine_similarity(left_empty, right_empty) == 0.0)
+    """
+        test cosine similarity for 2 empty lists
+    """
+    assert Metrics.cosine_similarity(LEFT_EMPTY, RIGHT_EMPTY) == 0.0
 
 def test_cosine_similarity_left_empty():
-    assert(Metrics.cosine_similarity(left_empty, right_populated_1) == 0.0)
+    """
+        test cosine similarity if the 1st parameter is empty
+    """
+    assert Metrics.cosine_similarity(LEFT_EMPTY, RIGHT_POPULATED_1) == 0.0
 
 def test_cosine_similarity_right_empty():
-    assert(Metrics.cosine_similarity(left_populated_1, right_empty) == 0.0)
+    """
+        test cosine similarity if the 2nd parameter is empty
+    """
+    assert Metrics.cosine_similarity(LEFT_POPULATED_1, RIGHT_EMPTY) == 0.0
 
 def test_cosine_similarity_populated_1():
-    assert(Metrics.cosine_similarity(left_populated_1, right_populated_1) == 1.0)
+    """
+        test cosine similarity for one pair of populated lists
+    """
+    assert Metrics.cosine_similarity(LEFT_POPULATED_1, RIGHT_POPULATED_1) == 1.0
 
 def test_cosine_similarity_populated_2():
-    assert(Metrics.cosine_similarity(left_populated_2, right_populated_2) == 0.338)
+    """
+        test cosine similarity for another pair of populated lists
+    """
+    assert Metrics.cosine_similarity(LEFT_POPULATED_2, RIGHT_POPULATED_2) == 0.338
 
 def test_jaccard_similarity_double_empty():
-    assert(Metrics.jaccard_similarity(left_empty, right_empty) == 0.0)
+    """
+        test jaccard similarity for 2 empty lists
+    """
+    assert Metrics.jaccard_similarity(LEFT_EMPTY, RIGHT_EMPTY) == 0.0
 
 def test_jaccard_similarity_left_empty():
-    assert(Metrics.jaccard_similarity(left_empty, right_populated_1) == 0.0)
+    """
+        test jaccard similarity if the 1st parameter is empty
+    """
+    assert Metrics.jaccard_similarity(LEFT_EMPTY, RIGHT_POPULATED_1) == 0.0
 
 def test_jaccard_similarity_right_empty():
-    assert(Metrics.jaccard_similarity(left_populated_1, right_empty) == 0.0)
+    """
+        test jaccard similarity if the 2nd parameter is empty
+    """
+    assert Metrics.jaccard_similarity(LEFT_POPULATED_1, RIGHT_EMPTY) == 0.0
 
 def test_jaccard_similarity_populated_1():
-    assert(Metrics.jaccard_similarity(left_populated_1, right_populated_1) == 1.0)
+    """
+        test jaccard similarity for one pair of populated lists
+    """
+    assert Metrics.jaccard_similarity(LEFT_POPULATED_1, RIGHT_POPULATED_1) == 1.0
 
 def test_jaccard_similarity_populated_2():
-    assert(Metrics.jaccard_similarity(left_populated_2, right_populated_2) == 0.2)
+    """
+        test jaccard similarity for another pair of populated lists
+    """
+    assert Metrics.jaccard_similarity(LEFT_POPULATED_2, RIGHT_POPULATED_2) == 0.2
+    
--- test/nlp/test_stemming_lemmatization.py
+++ test/nlp/test_stemming_lemmatization.py
@@ -4,18 +4,26 @@ Created on Nov 14, 2019
 @author: gregory
 '''
 
-from nlp import StemmingLemmatization, Tokenizer
+from nlp import Tokenizer
 
-phrase_1 = 'The fox was quickly walking by the seashore in the morning daylight.'
+PHRASE_1 = 'The fox was quickly walking by the seashore in the morning daylight.'
 
 def test_apply_nltk_porter_stemmer():
+    """
+        Test the porter stemmer for a standard sentence
+    """
     stemmer = 'Porter'
-    expected_output = ['the', 'fox', 'wa', 'quickli', 'walk', 'by', 'the', 'seashor', 'in', 'the', 'morn', 'daylight.']
-    tokens = Tokenizer.whitespace_tokenize(phrase_1, stemmer)
-    assert(expected_output == tokens)
+    expected_output = ['the', 'fox', 'wa', 'quickli', 'walk', 'by',
+                       'the', 'seashor', 'in', 'the', 'morn', 'daylight.']
+    tokens = Tokenizer.whitespace_tokenize(PHRASE_1, stemmer)
+    assert expected_output == tokens
 
 def test_apply_nltk_snowball_stemmer():
+    """
+        Test the snowball stemmer for a standard sentence
+    """
     stemmer = 'Snowball'
-    expected_output = ['the', 'fox', 'was', 'quick', 'walk', 'by', 'the', 'seashor', 'in', 'the', 'morn', 'daylight.']
-    tokens = Tokenizer.whitespace_tokenize(phrase_1, stemmer)
-    assert(expected_output == tokens)
+    expected_output = ['the', 'fox', 'was', 'quick', 'walk', 'by',
+                       'the', 'seashor', 'in', 'the', 'morn', 'daylight.']
+    tokens = Tokenizer.whitespace_tokenize(PHRASE_1, stemmer)
+    assert expected_output == tokens
